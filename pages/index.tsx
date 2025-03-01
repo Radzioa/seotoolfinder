f7b4c83dfe62eef43189177e79b9892c562f6de7
Radzioa <radzio.angel@gmail.com>
2025-03-01 23:19:53 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
-import React from 'react';
 import dynamic from 'next/dynamic';
 
-// Import dynamiczny zapobiega błędom podczas renderowania po stronie serwera
-const SEOToolFinder = dynamic(() => import('../components/SEOToolFinder'), {
+const SEOToolFinder = dynamic(() => import('../components/SEOToolFinder'), { 
   ssr: false,
-  loading: () => <p>Ładowanie narzędzia...</p>
-});
-
-export default function Home() {
-  return (
-    <div className="container mx-auto p-6">
-      <SEOToolFinder />
-    </div>
-  );
-}
+  loading
